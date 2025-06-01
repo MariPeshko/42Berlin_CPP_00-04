a116ba6f582d34ec5920ab4a8d7502f68dd4aa28
MariPeshko <marunapeshko@gmail.com>
2025-06-02 00:34:40 +0200

    finished operator overloaded

--- CPP02/operator_overload/Integer.class.cpp
+++ CPP02/operator_overload/Integer.class.cpp
@@ -13,7 +13,7 @@ Integer::~Integer( void ) {
     return;
 }
 
-int	getValue(void) const {
+int	Integer::getValue(void) const {
 
 	return this->_n;
 
@@ -24,10 +24,21 @@ Integer	&	Integer::operator=(Integer const & rhs) {
        std::cout << "Assignation operator called from " << this->_n;
 	   std::cout << " to " << rhs.getValue() << std::endl;
 
-	   this._n = rhs.getValue();
-	   return *this;
+	   this->_n = rhs.getValue();
+	   return *this; // unreference my pointer
 };
 
 Integer		Integer::operator+( Integer const & rhs ) const {
-	....
+	
+	std::cout << "Addition operator called with " << this->_n;
+	std::cout << " and " << rhs.getValue() << std::endl;
+	return Integer( this->_n + rhs.getValue() );
+
 };
+
+std::ostream &	operator<<( std::ostream & o, Integer const & rhs ) {
+
+	o << rhs.getValue();
+	return o;
+
+}
